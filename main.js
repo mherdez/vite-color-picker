844f131b034209617c5957b9ea748702847c79d3
Manuel Herdez <herdez@gmail.com>
2023-05-10 21:19:56 -0600

    Alpha 1.0

--- main.js
+++ main.js
@@ -1,17 +1,65 @@
-import './style.css'
-import ColorThief from 'colorthief/dist/color-thief.modern.mjs'
-
+import ColorThief from 'colorthief/dist/color-thief.modern.mjs';
+import './style.css';
 
 const container = document.querySelector('#app');
 const colorThief = new ColorThief();
-const img = new Image();
 
-container.appendChild(img);
+const num = () => Math.floor(Math.random() * 1084 + 1)
 
-img.addEventListener('load', function() {
-  const [r,g,b] = colorThief.getColor(img);
-  document.body.style.backgroundColor = `rgb(${r},${g},${b})`
-});
+const getImage = () => {
+  container.innerHTML = ''
 
-img.crossOrigin = 'Anonymous';
-img.src = 'https://picsum.photos/200/300'
+  // crea la imagen
+  const img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.src = `https://picsum.photos/id/${num()}/400`;
+  img.classList.add('imagen');
+
+  container.append(img);
+
+  // Escucha si la imagen no se carga y obtiene una nueva imagen
+  img.addEventListener('error', getImage)
+
+  // Inicia proceso colorThief con la imagen cargada correctamente
+  img.addEventListener('load', () => {
+
+    // obtiene el color dominante y cambia el background del body
+    const [r, g, b] = colorThief.getColor(img);
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    // obtiene los colores de la paleta
+    const palette = colorThief.getPalette(img);
+
+    const boxContainer = document.createElement('div');
+    boxContainer.classList.add('box-container');
+
+    container.append(boxContainer);
+
+    // crea los cuadros de la paleta de colores
+    palette.map(box => {
+      const div = document.createElement('div');
+      div.classList.add('box');
+
+      const [r, g, b] = box;
+      div.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+      boxContainer.append(div);
+
+      // detecta cuando el puntero del mouse entra a los cuadros
+      // de la paleta y cambia el background del body
+      div.addEventListener('mouseenter', () => {
+        document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+      });
+      div.addEventListener('click', () => {
+        document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+      });
+    });
+
+  });
+
+  // detecta el click en la imagen y genera una nueva
+  img.addEventListener('click', getImage)
+}
+
+// Carga la primera imagen cuando la carga del DOM esta compeltada
+document.addEventListener('DOMContentLoaded', getImage)
